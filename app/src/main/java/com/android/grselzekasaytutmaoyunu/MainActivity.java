d0dedabe3da4c87e5fc9909cfb435dee4de1ee04
Muhammet Saki Türüt <msakiturut@gmail.com>
2020-10-13 13:18:50 +0300

    Commit

--- app/src/main/java/com/android/grselzekasaytutmaoyunu/MainActivity.java
+++ app/src/main/java/com/android/grselzekasaytutmaoyunu/MainActivity.java
@@ -3,12 +3,27 @@ package com.android.grselzekasaytutmaoyunu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView mTextField;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextField = (TextView)findViewById(R.id.textView);
+
+        new CountDownTimer(30000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                mTextField.setText("seconds remaining: " + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                mTextField.setText("done!");
+            }
+        }.start();
     }
 }
