8c216f23c3ab23b89983d9d2af0f6cb05d3352ff
Muhammet Saki Türüt <msakiturut@gmail.com>
2020-10-14 10:38:48 +0300

    ....

--- app/src/main/java/com/android/grselzekasaytutmaoyunu/SayiGosterActivity.java
+++ app/src/main/java/com/android/grselzekasaytutmaoyunu/SayiGosterActivity.java
@@ -8,34 +8,127 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class SayiGosterActivity extends AppCompatActivity {
-    TextView textView;
-    Button buton;
+    TextView textView_timer;
+    Button b1,b2,b3,b4,b5,b6,b7,b8,b9;
+    EditText editText_number;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sayi_goster);
 
-        textView = findViewById(R.id.textView_timer);
+        textView_timer = findViewById(R.id.textView_timer);
+
+        editText_number=findViewById(R.id.editText);
+
+        b1=findViewById(R.id.button_one);
+        b2=findViewById(R.id.button_two);
+        b3=findViewById(R.id.button_three);
+        b4=findViewById(R.id.button_four);
+        b5=findViewById(R.id.button_five);
+        b6=findViewById(R.id.button_six);
+        b7=findViewById(R.id.button_seven);
+        b8=findViewById(R.id.button_eight);
+        b9=findViewById(R.id.button_nine);
 
 
         new CountDownTimer(5000, 1000) {
             int time;
             public void onTick(long millisUntilFinished) {
                 time = (int) (millisUntilFinished / 1000);
-                textView.setText(String.valueOf(time-1));
+                textView_timer.setText(String.valueOf(time-1));
             }
 
             public void onFinish() {
                 time -=1 ;
-                textView.setText(String.valueOf(time));
+                textView_timer.setText(String.valueOf(time));
                 toastMessage("-10");
-                finish();
+               // finish();
             }
         }.start();
+
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText_number.append("1");
+            }
+        });
+
+
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText_number.append("2");
+            }
+        });
+
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText_number.append("3");
+            }
+        });
+
+
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText_number.append("4");
+            }
+        });
+
+
+
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText_number.append("5");
+            }
+        });
+
+
+
+        b6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText_number.append("6");
+            }
+        });
+
+
+
+        b7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText_number.append("7");
+            }
+        });
+
+
+
+        b8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText_number.append("8");
+            }
+        });
+
+
+
+        b9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText_number.append("9");
+            }
+        });
+
 
        /* buton.setOnClickListener(new View.OnClickListener() {
             @Override
